e515fe10d0f74bb81d461ccd6eabc87fa5d4e924
Ameth Toledo <233363@ids.upchiapas.edu.mx>
2025-12-17 23:06:45 -0600

    section for languajes

--- src/app/features/landing/pages/home/landing.component.ts
+++ src/app/features/landing/pages/home/landing.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "../../components/hero/hero.component";
+import { HeaderComponent } from '../../../../shared/components/header/header.component';
+import { TechLanguagesGridComponent } from "../../components/tech-languages-grid/tech-languages-grid.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, HeaderComponent, TechLanguagesGridComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
