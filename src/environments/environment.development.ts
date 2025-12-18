808b67e7a0a8bdf63e153fc73dfb384b62e877d1
Ameth Toledo <233363@ids.upchiapas.edu.mx>
2025-12-17 18:17:44 -0600

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api',
+  apiVersion: 'v1',
+  cloudinaryCloudName: 'tu-cloud-name',
+  cloudinaryUploadPreset: 'tu-upload-preset',
+  enableDebugLogs: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.tudominio.com/api',
+  apiVersion: 'v1',
+  cloudinaryCloudName: 'tu-cloud-name',
+  cloudinaryUploadPreset: 'tu-upload-preset',
+  enableDebugLogs: false
+};
